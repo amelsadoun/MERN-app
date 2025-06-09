401ff7ddc7fed57fd31a91f605ec381ff30f43b3
amel sadoun <amelsadoun.b@gmail.com>
2025-06-09 18:15:13 +0100

    changed endpoints after hosting backend

--- client/MERN app/src/api/index.js
+++ client/MERN app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const port = import.meta.env.PORT || 5000;
-const events_url = `http://localhost:${port}/api/events`;
+const events_url = `https://mern-app-uvft.onrender.com/api/events`;
 
 //event posting and getting (?)
 export const fetchEvents = (filters, searchQuery, page = 1, limit = 10) =>
@@ -21,7 +21,7 @@ export const updateEvent = (id, event) =>
 
 // auth
 // WHY IS IT THAT WHEN I PUT THE CONST IT DOESNT WOOORK
-const auth_url = `http://localhost:${port}/api/auth`;
+const auth_url = `https://mern-app-uvft.onrender.com/api/auth`;
 
 //authorization functions
 export const login = (clubData) => axios.post(`${auth_url}/login`, clubData);
@@ -31,7 +31,7 @@ export const updateProfile = (id, club) =>
 export const changePassword = (id, passwords) =>
   axios.put(`${auth_url}/changePassword/${id}`, passwords);
 
-const clubs_url = `http://localhost:${port}/api/clubs`;
+const clubs_url = `https://mern-app-uvft.onrender.com/api/clubs`;
 
 //getting club info for profile
 export const getClub = (id) => axios.get(`${clubs_url}/${id}`);
